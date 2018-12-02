86b1fe4271eb1d6753ae60b8a3bbab1e7bb148b4
Timothy Tan <timtan93@gmail.com>
2018-12-02 15:09:00 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -1,6 +1,7 @@
 def using_push(array, string)
   array.push(string)
 end
+<<<<<<< HEAD
 
  def using_unshift(array, string)
   updated_array = array.unshift(string)
@@ -38,10 +39,53 @@ end
   array.flatten
 end
 
+=======
+ 
+ def using_unshift(array, string)
+  updated_array = array.unshift(string)
+end
+ 
+ def using_pop(array)
+  array.pop
+end
+ 
+ def pop_with_args(array)
+  return array.pop(2)
+end
+ 
+ def using_shift(array)
+  array.shift
+end
+ 
+ def shift_with_args(array)
+  array.shift(2)
+end
+ 
+ def using_concat(array1, array2)
+  array1.concat(array2)
+end
+ 
+ def using_insert(array, element)
+  array.insert(4, element)
+end
+ 
+ def using_uniq(array)
+  array.uniq
+end
+ 
+ def using_flatten(array)
+  array.flatten
+end
+ 
+>>>>>>> 823b4c04a5567ebd7b3ae27c2d76453502cdf4cb
  def using_delete(array, string)
   array.delete(string)
 end
 
  def using_delete_at(array, int)
   array.delete_at(int)
+<<<<<<< HEAD
 end
+=======
+end
+>>>>>>> 823b4c04a5567ebd7b3ae27c2d76453502cdf4cb
